15b7dbdaed15dc8b825da352da5f60e336629861
freccia <Freccia@users.noreply.github.com>
2018-10-04 17:21:39 +0200

    norme

--- include/error.h
+++ include/error.h
@@ -6,12 +6,12 @@
 /*   By: lfabbro <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/25 12:42:18 by lfabbro           #+#    #+#             */
-/*   Updated: 2018/08/25 12:43:14 by lfabbro          ###   ########.fr       */
+/*   Updated: 2018/10/04 17:21:11 by lfabbro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_ERROR
-# define FT_ERROR
+#ifndef FT_ERROR_H
+# define FT_ERROR_H
 
 # define FAIL_MALLOC	"Malloc failed."
 
